9fa4ab3e3e10ad5b964a62f0d93023c123c82791
PriZm23 <drumfordumb@mail.ru>
2021-09-30 16:39:27 +1000

    Aim Firing

---
+++ Assets/ShipTarget.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipTarget : MonoBehaviour
+{
+    // Спрайт для использования в качестве прицельной сетки
+    public Sprite targetImage;
+
+    void Start()
+    {
+        // Зарегистрировать новый индикатор, соответствующий
+        // данному объекту, использовать жёлтый цвет
+        // и нестандартный спрайт
+        IndicatorManager.instance.AddIndicator(gameObject, Color.yellow, targetImage);
+    }
+
+    void Update()
+    {
+        
+    }
+}
